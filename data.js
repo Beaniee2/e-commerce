fb8b06af794461ca747195588e1030ffc3524d46
Hana Maling-Smith <144044444+Beaniee2@users.noreply.github.com>
2024-06-08 18:19:22 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+let shopItemsData = [
+    {
+      id:"item1",
+      name:"Limited edition trainers",
+      price: 125, 
+      desc:"These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubberouter sole, they'll withstand everything the weather can offer",
+      img: "image-product-1.jpg",
+    }];
